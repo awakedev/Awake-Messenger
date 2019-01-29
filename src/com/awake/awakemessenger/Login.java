28a19e995c7a82171af1c3bbad8fa5274ef5dd20
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-29 16:16:48 +0000

    client design

--- src/com/awake/awakemessenger/Login.java
+++ src/com/awake/awakemessenger/Login.java
@@ -3,11 +3,15 @@ package com.awake.awakemessenger;
 import java.awt.EventQueue;
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
@@ -15,9 +19,23 @@ public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtName;
+	private JLabel lblIP;
+	private JTextField txtIP;
+	private JLabel lblPort;
+	private JTextField txtPort;
+	private JLabel lblPortDesc;
+	private JLabel lblAddressDesc;
 
 	
 	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		setResizable(false);
 		setFont(new Font("Adobe Gurmukhi", Font.PLAIN, 12));
 		setTitle("Login");
@@ -35,8 +53,52 @@ public class Login extends JFrame {
 		txtName.setColumns(10);
 		
 		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(124, 18, 46, 14);
+		lblName.setBounds(24, 46, 46, 14);
 		contentPane.add(lblName);
+		
+		lblIP = new JLabel("IP Address:");
+		lblIP.setBounds(10, 102, 89, 14);
+		contentPane.add(lblIP);
+		
+		txtIP = new JTextField();
+		txtIP.setColumns(10);
+		txtIP.setBounds(80, 99, 133, 20);
+		contentPane.add(txtIP);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(24, 166, 46, 14);
+		contentPane.add(lblPort);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(80, 160, 133, 20);
+		contentPane.add(txtPort);
+		
+		lblPortDesc = new JLabel("(e.g 8192)");
+		lblPortDesc.setBounds(109, 184, 75, 14);
+		contentPane.add(lblPortDesc);
+		
+		lblAddressDesc = new JLabel("(e.g 192.168.0.2)");
+		lblAddressDesc.setBounds(95, 127, 104, 14);
+		contentPane.add(lblAddressDesc);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = txtName.getText();
+				String address = txtIP.getText();
+				int port = Integer.parseInt(txtPort.getText());
+				login(address, address, port);
+			}
+
+			
+		});
+		btnLogin.setBounds(102, 251, 89, 23);
+		contentPane.add(btnLogin);
+	}
+	
+	private void login(String name, String address, int port) {
+		dispose();
 	}
 	
 	
